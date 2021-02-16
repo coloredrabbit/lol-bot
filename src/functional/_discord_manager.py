89cbfa2d83d6f295d0d79a66cd8c8e8633efb9be
kimdainn <cd8025@nate.com>
2021-02-16 13:22:40 +0900

    sys exit and exception

--- src/functional/_discord_manager.py
+++ src/functional/_discord_manager.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 #다인
 import random
+import sys
 
 app = commands.Bot(command_prefix='!')
 participants = set()
@@ -64,23 +65,33 @@ async def 삭제(ctx, *, text):
 #TODO 김다인: random
 @app.command()
 async def mix_random(ctx, *, text):
-    randomParticipants = list(participants)
-    number_of_people = len(randomParticipants)
-    number_of_teams = int(number_of_people / 5) # 팀 개수
-
-    random.shuffle(randomParticipants) # 팀 랜덤으로 섞기
-    number_of_rest = int(number_of_people % 5) # 팀 나누고 나머지 인원
-    team_group = [] # 랜덤 팀
-
-    for i in range(0,number_of_teams):
-        team_group.append(randomParticipants[0:5])
-        del randomParticipants[0:5]
+    # 서버 끄기
+    if text == '종료':
+        await ctx.send('시스템을 종료합니다')
+        sys.exit()
     
-    # 팀이 나누어 떨어지지 않을 때
-    if number_of_rest != 0:
-        team_group.append(randomParticipants[0:number_of_rest])
+    if not participants:
+        await ctx.send('!참가 명령으로 내전에 참가할 인원을 먼저 추가해주세요')
 
-    await ctx.send(team_group)
+    else :
+
+        randomParticipants = list(participants)
+        number_of_people = len(randomParticipants)
+        number_of_teams = int(number_of_people / 5) # 팀 개수
+
+        random.shuffle(randomParticipants) # 팀 랜덤으로 섞기
+        number_of_rest = int(number_of_people % 5) # 팀 나누고 나머지 인원
+        team_group = [] # 랜덤 팀
+
+        for i in range(0,number_of_teams):
+            team_group.append(randomParticipants[0:5])
+            del randomParticipants[0:5]
+        
+        # 팀이 나누어 떨어지지 않을 때
+        if number_of_rest != 0:
+            team_group.append(randomParticipants[0:number_of_rest])
+
+        await ctx.send(team_group)
 
 @app.command()
 async def 랜덤(ctx, *, text):
